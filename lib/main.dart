bb227e09daa6ffa72762dce7411fb3d05b4a892c
Yana Mulyana <yanamyn15@gmail.com>
2023-12-13 22:49:56 +0700

    multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:state_management_provider/main_model.dart';
+import 'package:state_management_provider/second_counter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => MainModel(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -34,33 +38,43 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => MainModel(),
-      child: Consumer<MainModel>(
-        builder: (context, model, child) => Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: Text(widget.title),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
+    return Consumer<MainModel>(
+      builder: (context, model, child) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '${model.counter}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: TextButton(
+                  child: const Text("Open counter 2"),
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          const SecondCounter(title: "Second Counter"),
+                    ),
+                  ),
                 ),
-                Text(
-                  '${model.counter}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
+              )
+            ],
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => model.counter++,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => model.counter++,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
         ),
       ),
     );
---
+++ lib/second_counter.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management_provider/main_model.dart';
+
+class SecondCounter extends StatefulWidget {
+  const SecondCounter({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<SecondCounter> createState() => _SecondCounterState();
+}
+
+class _SecondCounterState extends State<SecondCounter> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MainModel>(
+      builder: (context, model, child) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '${model.counter}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => model.counter++,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
