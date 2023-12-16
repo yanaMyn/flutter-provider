40bd936b789b94c7733b606dcdaac37c6b5ffc41
Yana Mulyana <yanamyn15@gmail.com>
2023-12-16 21:54:39 +0700

    add provider screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:state_management_provider/main_model.dart';
+import 'package:state_management_provider/provider_screen.dart';
 import 'package:state_management_provider/second_counter.dart';
 
 void main() {
@@ -36,40 +37,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("_MyHomePageState");
     return Consumer<MainModel>(
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '${model.counter}',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: TextButton(
-                  child: const Text("Open counter 2"),
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          const SecondCounter(title: "Second Counter"),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
+        body: const ProviderScreen(),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedPageIndex,
+          onTap: (value) => _selectPage(value),
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: "provider"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.star), label: "Future Provider"),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => model.counter++,
---
+++ lib/provider_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management_provider/main_model.dart';
+import 'package:state_management_provider/second_counter.dart';
+
+class ProviderScreen extends StatelessWidget {
+  const ProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print("ProviderScreen");
+    MainModel model = Provider.of<MainModel>(context, listen: true);
+    return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '${model.counter}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: TextButton(
+                  child: const Text("Open counter 2"),
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          const SecondCounter(title: "Second Counter"),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+  }
+}
